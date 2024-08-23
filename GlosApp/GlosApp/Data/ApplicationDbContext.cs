4c2beb628c7bea0992bc733491de94d8a6976f68
Edvin Thörnholm <edvin.thornholm@gmail.com>
2024-08-23 11:54:55 +0200

    Done white words and login

--- GlosApp/GlosApp/Data/ApplicationDbContext.cs
+++ GlosApp/GlosApp/Data/ApplicationDbContext.cs
@@ -1,10 +1,27 @@
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+//using GlosApp.Models;
+
+//namespace GlosApp.Data
+//{
+//    public class ApplicationDbContext : IdentityDbContext
+//    {
+//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        public DbSet<WordAnswer> WordAnswers { get; set; }
+//    }
+//}
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using GlosApp.Models;
 
 namespace GlosApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -12,5 +29,13 @@ namespace GlosApp.Data
         }
 
         public DbSet<WordAnswer> WordAnswers { get; set; }
+
+        // Överskriv OnModelCreating om du behöver konfigurera entity mappings
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Ytterligare konfiguration om det behövs
+        }
     }
 }
+
