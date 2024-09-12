73da2dda0fe44904c96baf96ec0a77663c7c52d4
Edvin Thörnholm <edvin.thornholm@gmail.com>
2024-09-12 09:36:16 +0200

    update quiz

---
+++ GlosApp/GlosApp/Models/QuizQuestion.cs
@@ -0,0 +1,13 @@
+﻿namespace GlosApp.Models;
+
+public class QuizQuestion
+{
+    public string QuestionText { get; set; }
+    public List<QuizOption> Options { get; set; }
+    public string CorrectAnswer { get; set; }
+}
+
+public class QuizOption
+{
+    public string OptionText { get; set; }
+}
